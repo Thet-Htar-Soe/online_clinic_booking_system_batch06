aef198a169eadf2925240a07acb230a97e703eb3
thethtarsoe <thethtarsoe2761@gmail.com>
2023-01-09 15:12:25 +0630

    feature/homepage fix

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('patient');
 })->name("home");
 
 Route::get('/admin', function () {
