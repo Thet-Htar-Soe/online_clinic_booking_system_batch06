ab7684813bb681d5f211b296a9b15e912d476b96
amimihtun <leonwhit22@gmail.com>
2023-01-11 12:24:34 +0630

    patient migration

---
+++ database/migrations/2023_01_11_053354_create_patients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('phone');
+            $table->integer('age');
+            $table->char('gender')->comment('m:male, f:female, k:unknown, a:unapplicable');
+            $table->text('address');
+            $table->enum('blood_type', ['A','B','O']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
