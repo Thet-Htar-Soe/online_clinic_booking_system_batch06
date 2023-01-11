0897c111eb89935b2addb9472f4b61cd19375850
johnpyaephyoaung <pyaephyoaung31999@gmail.com>
2023-01-11 14:48:32 +0630

    feature/admin db seed

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,10 +5,10 @@ namespace Database\Seeders;
 use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
